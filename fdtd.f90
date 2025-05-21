72585f93e6909b9af9cd3f7e35176d21f199aba1
ALI Emin <eminali2430@gmail.com>
2025-05-21 15:37:23 +0200

    Affichage matrices

--- fdtd.f90
+++ fdtd.f90
@@ -125,11 +125,11 @@ MODULE fdtd
                   A1(Nx, Nx - 1) = cn%a1 / cn%dy
                   A1(Nx, Nx) = 1.0d0
 
-            ! ! Affichage de la matrice A1
-            ! WRITE(*, '(/, T5, A, /)') "Matrice A1 :"
-            ! DO i = 0, Nx
-            !       WRITE(*, '(I5,500F12.2)') i, A1(i,:)
-            ! END DO
+            ! Affichage de la matrice A1
+            WRITE(*, '(/, T5, A, /)') "Matrice A1 :"
+            DO i = 0, Nx
+                  WRITE(*, '(I5,500F12.2)') i, A1(i,:)
+            END DO
             
 
 
@@ -150,11 +150,11 @@ MODULE fdtd
                   A2(Nx, Nx) = 1.0d0
 
 
-            ! ! Affichage de la matrice A2
-            ! WRITE(*, '(/, T5, A, /)') "Matrice A2 :"
-            ! DO i = 0, Nx
-            !       WRITE(*, '(I5,500F12.2)') i + Nx + 1, A2(i,:)
-            ! END DO
+            ! Affichage de la matrice A2
+            WRITE(*, '(/, T5, A, /)') "Matrice A2 :"
+            DO i = 0, Nx
+                  WRITE(*, '(I5,500F12.2)') i + Nx + 1, A2(i,:)
+            END DO
 
 
 
@@ -187,11 +187,11 @@ MODULE fdtd
                   A3(Nx, Nx - 1) =   cn%a2 / cn%dx
                   A3(Nx - 1, Nx) = - cn%a2 / cn%dy
 
-            ! ! Affichage de la matrice A3
-            ! WRITE(*, '(/, T5, A, /)') "Matrice A3 :"
-            ! DO i = 0, Nx
-            !       WRITE(*, '(I5,500F12.5)') i + 2 * (Nx + 1), A3(i,:)
-            ! END DO
+            ! Affichage de la matrice A3
+            WRITE(*, '(/, T5, A, /)') "Matrice A3 :"
+            DO i = 0, Nx
+                  WRITE(*, '(I5,500F12.5)') i + 2 * (Nx + 1), A3(i,:)
+            END DO
             
              
             !---------------------------------------------------------------!
@@ -242,12 +242,12 @@ MODULE fdtd
             !STOP 'LU decomposition error'
 
 
-            ! ! ! Affichage de la matrice A
-            ! WRITE(*, '(/, T5, A, /)') "Matrice A :"
-            ! DO i = 0, 3 * Nx + 2
-            !       WRITE(*, '(I5,500F12.5)') i, cn%A(i,:)
-            ! END DO
-            ! ! !---------------------------------------------------!
+            ! ! Affichage de la matrice A
+            WRITE(*, '(/, T5, A, /)') "Matrice A :"
+            DO i = 0, 3 * Nx + 2
+                  WRITE(*, '(I5,500F12.5)') i, cn%A(i,:)
+            END DO
+            ! !---------------------------------------------------!
 
 
             
