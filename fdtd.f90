31804b3ccb37845dac6a680353f4d2fe0af18e9a
ALI Emin <eminali2430@gmail.com>
2025-10-03 17:02:46 +0200

    Imlpémentation de la foonction d'indexation pour les variables Ex, Ey, Hz dans une grille FDTD 2D

--- fdtd.f90
+++ fdtd.f90
@@ -79,42 +79,38 @@ MODULE fdtd
 
       END SUBROUTINE init
 
+      FUNCTION function_idx(i,j,n_var)
+            INTEGER, INTENT(in) :: i,j,n_var
+            INTEGER :: function_idx
+
+            function_idx = (n_var-1) * (Nx + 1) * (Ny + 1) + j * (Nx + 1) + i
+            RETURN
+      END FUNCTION function_idx
+
       SUBROUTINE compute_fdtd(cn)
             CLASS(cnfdtd), INTENT(inout) :: cn
             ! Variables locales
             CHARACTER(LEN=500) :: charac 
             LOGICAL :: display_it
             INTEGER :: info
-            INTEGER :: n, m, nrhs, nvec, nrow, ncol
-            INTEGER :: i,j
+            INTEGER :: n, m, n_var, n_node
+            INTEGER :: i,j, idx,i_var
             INTEGER :: i0,j0,i1,j1
             INTEGER :: snapshot
-            INTEGER :: idx_Hx, idx_Hy
-            REAL(8), ALLOCATABLE :: Hxx(:,:)
-            REAL(8), ALLOCATABLE :: Hyy(:,:)
-            REAL(8), ALLOCATABLE :: Hxy(:,:)
-            REAL(8), ALLOCATABLE :: Hyx(:,:)
-            REAL(8), ALLOCATABLE :: B_pec(:,:)
+            INTEGER, ALLOCATABLE :: A(:, :)
 
-            INTEGER :: ipiv(SIZE(cn%A,1))       ! Sert de pivot
+            ! Variable d'indices pour 3 variables inconnus Ex, Ey, Hz de dimension (Nx+1)*(Ny+1) chacune
+            n_var = 3
+            
 
-            ALLOCATE(Hxx(0:Nx, 0:Ny))
-            ALLOCATE(Hyy(0:Nx, 0:Ny))
-            ALLOCATE(Hxy(0:Nx, 0:Ny))
-            ALLOCATE(Hyx(0:Nx, 0:Ny))
-            ALLOCATE(B_pec(0:2 * Nx + 1, 0:Ny))
-
-            Hxx = 0.d0; Hyy = 0.d0; Hxy = 0.d0; Hyx = 0.d0; B_pec = 0.d0; ipiv = 0
-
-            WRITE(*,'(/,T5,A,I5)') "Nx = ", Nx
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(A) = "    ,  shape(cn%A)
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(A_i) = "  ,  shape(Hxx)
-            WRITE(*,'(/, T5, A, I15X)')    "shape(rhs) = "    ,  shape(cn%rhs)
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(B_pec) = ",  shape(B_pec)
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(Hx) = "   ,  shape(cn%Hx)
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(Hy) = "   ,  shape(cn%Hy)
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(Ez) = "   ,  shape(cn%Ez)
-            WRITE(*,'(/, T5, A, I5X, I5)') "shape(ipiv) = " ,  shape(ipiv)
+            DO i_var = 1, n_var
+                  DO j = 0, Ny
+                        DO i = 0, Nx
+                              idx = function_idx(i,j,i_var)
+                              WRITE(*,'(3(AX,I5))') 'i=',i,' j=',j,' idx=',idx
+                        END DO  
+                  END DO
+            END DO
 
             
             m = 0
@@ -128,303 +124,25 @@ MODULE fdtd
             !------------------ Ecriture de la matrice A -----------------!
             !-------------------------------------------------------------!
 
-                        ! -------- ! -------- !
-                        !   Hxx    !   Hxy    !
-            ! A =       !----------!----------!
-                        !   Hyx    !   Hyy    !
-                        ! -------- ! -------- !
+                        ! -------- ! -------- !-------!
+                        !   A11    !   A12    !  A13  !
+            ! A =       !----------!----------!-------!
+                        !   A21    !   A22    !  A23  !
+                        ! -------- ! -------- !-------!
+                        !   A31    !   A32    !  A33  !
+                        ! -------- ! -------- !-------!
 
-            !----------------------------------------------------!
-            !------------------ Sous matrice Hxx -----------------!
-            !----------------------------------------------------!
-                  Hxx(0,0) = 1.0d0 + 2.d0 * cn%bx**2
-                  Hxx(0,1) = - cn%bx**2 
 
-                  DO j = 1, Nx
-                        Hxx(j,j-1) = - cn%bx**2
-                        Hxx(j,j) = 1.0d0 + 2.d0 * cn%bx**2
-                        Hxx(j,j+1) = - cn%bx**2
-                  END DO
 
 
 
-            ! ! Affichage de la matrice Hxx
-            IF (display_it) THEN
-                  CALL display_matrix(Hxx, "Hxx")
-            END IF
 
-            
 
 
 
-            !----------------------------------------------------!
-            !------------------ Sous matrice Hyy -----------------!
-            !----------------------------------------------------!     
-            Hyy = Hxx
 
-            ! ! Affichage de la matrice Hyy
-            IF (display_it) THEN
-                  CALL display_matrix(Hyy, "Hyy")
-            END IF
 
 
-
-
-            !----------------------------------------------------!
-            !------------------ Sous matrice Hxy -----------------!
-            !----------------------------------------------------!
-
-                  DO i = 0, Nx-2
-                        j = i
-                        Hxy(i , j)     =  -1.d0
-                        Hxy(i , j + 1) =   2.d0
-                        Hxy(i , j + 2) =  -1.d0
-                  END DO 
-
-                  Hxy(Nx-1,Nx-1)    = - 1.d0
-                  Hxy(Nx-1, Nx)     =   2.d0
-                  Hxy(Nx, Nx)       = - 1.d0
-
-                  Hxy = - cn%bx * cn%by * Hxy
-                  
-
-
-
-           
-
-            !----------------------------------------------------!
-            !------------------ Sous matrice Hyx -----------------!
-            !----------------------------------------------------!
-
-                  Hyx = transpose(Hxy)
-                  Hxy = Hyx
-                  Hyx = transpose(Hxy)
-
-
-
-
-             ! ! Affichage de la matrice Hxy
-            IF (display_it) THEN
-                  CALL display_matrix(Hxy, "Hxy")
-            END IF
-
-            ! Affichage de la matrice Hyx
-            IF (display_it) THEN
-                  CALL display_matrix(Hyx, "Hyx")
-            END IF
-            
-
-                  
-            
-             
-            !---------------------------------------------------------------!
-            !------------------ Assemblage de la matrice A -----------------!
-            !---------------------------------------------------------------!
-
-                        ! -------- ! -------- !
-                        !   Hxx    !   Hxy    !
-            ! A =       !----------!----------!
-                        !   Hyx    !   Hyy    !
-                        ! -------- ! -------- !
-
-
-            ! Détermine les indices de collage
-            i0 = 0;        j0 = 0
-            i1 = Nx + 1;   j1 = Ny + 1
-            
-
-            WRITE(*, '(/, T5, A, I5, I5)') "i0, j0 = ", i0, j0
-            WRITE(*, '(/, T5, A, I5, I5)') "i1, j1 = ", i1, j1
-
-            ! Collage des blocs diagonaux
-            cn%A(i0  :i0 + Nx, j0  :j0 + Ny)   = Hxx
-            cn%A(i1  :i1 + Nx, j1  :j1 + Ny)   = Hyy
-            ! Collage des matrices de couplage
-            cn%A(i0 : i0 + Nx, j1 : j1 + Ny) = Hxy
-            cn%A(i1 : i1 + Nx, j0 : j0 + Ny) = Hyx
-
-
-
-
-
-            IF (display_it) then
-                  CALL display_matrix(cn%A, " A assemblée")
-            ENDIF
-
-            ! Vérification de la symétrie de la matrice A
-            !CALL matrix_sym(cn%A)
-            ! ! !---------------------------------------------------!
-
-            ! Libération de mémoire 
-            IF (ALLOCATED(Hxx)) THEN
-                  DEALLOCATE(Hxx)
-            END IF
-            IF (ALLOCATED(Hyy)) THEN
-                  DEALLOCATE(Hyy)
-            END IF
-            IF (ALLOCATED(Hxy)) THEN
-                  DEALLOCATE(Hxy)
-            END IF
-            IF (ALLOCATED(Hyx)) THEN
-                  DEALLOCATE(Hyx)
-            END IF
-            ! ! !---------------------------------------------------!
-
-
-
-
-            ! -------------------------------------------------------------------!
-            ! ------------------ Décomposition LU de A --------------------!
-            ! -------------------------------------------------------------------!
-
-            CALL DGETRF(size(cn%A,1), SIZE(cn%A,2),cn%A, size(cn%A,1),ipiv, info)
-            !CALL DGETRF(size(A_int,1), size(A_int,2), A_int1, size(A_int,1), ipiv, info)
-            IF (info > 0) THEN
-                  WRITE(*,'(/,T5,A,I0,A,I0,A,/)') 'U(', info , ',', info ,') is exactly zero. The factorization has been completed, but the factor U is exactly singular.'
-                  STOP 'LU failed'
-            ELSE IF (info < 0) THEN
-                  WRITE(*,'(T5,A,I0,A,/)') 'The ',info,'-th argument had an ilegal value.'
-                  STOP 'LU failed'
-            END IF
-            
-
-            
-
-
-            ! Ouverture du fichier de sortie
-            OPEN(idfile , file = "data/Hx.txt", status = "replace", action = "write", form = "formatted")
-            OPEN(idfile + 1 , file = "data/Ez.txt", status = "replace", action = "write", form = "formatted")
-            
-            !-------------------------------------------------------------!
-            !------------------- Boucle temporelle -----------------------!
-            !-------------------------------------------------------------!
-            WRITE(*, '(/, T5, "Injection de la source en ", I5, I5)') i_src, j_src
-            WRITE(*, '(/, T5, A, /)') "Début de la boucle temporelle"
-            snapshot = 10
-
-            nrow = 2 * (Nx - 1)
-            ncol = Ny - 1
-            nvec = nrow * ncol
-            nrhs = 1
-            m = 0
-            DO n = 0, Nt - 1
-
-                  IF (MOD(n,5*snapshot) == 0) THEN
-                        WRITE(*, '(/, T5, "itération temporelle : ",I4)') n
-                  END IF
-
-                  !--------------------------------------------------------------!
-                  !------------------- Ecriture du vecteur rhs ------------------!
-                  !--------------------------------------------------------------!
-                   ! On enregistre les résultats du temps n
-                  cn%Hx = B_pec(0 : Nx, 0 : Ny)
-                  cn%Hy = B_pec(i1 : i1 + Nx, 0 : Ny)                   !i1 = Nx + 1
-                  
-
-                  ! On parcourt l'entierté des champs Hx et Hy
-                  ! Second membre Hx
-                  DO i = 1,  Nx-1
-                        !print *, "i = ", i)
-                        DO j = 1, Ny-1
-                              ! Détermine le bonne indice
-                              idx_Hx = i * (Nx + 1) + j
-                              !print *, "idx_Hx = ", idx_Hx, "i,j =", i , j
-                              cn%rhs(idx_Hx) =      (1.d0 - 2.d0 * cn%bx**2) * cn%Hx(i,j)                       & 
-                                          + cn%bx**2 * ( cn%Hx(i, j - 1) + cn%Hx(i, j + 1) )                    &
-                                          - cn%bx*cn%by * ( cn%Hy(i + 1,  j)     - cn%Hy(i, j) )                &
-                                          + cn%bx*cn%by * ( cn%Hy(i + 1 , j - 1) - cn%Hy(i, j - 1) )            &
-                                          - 2.d0 * cn%a2 * (cn%Ez(i,j) - cn%Ez(i, j-1))
-                        END DO
-                  END DO
-
-                  ! ! Conditoon aux bords du champ magnétique
-                  ! DO i = 0, Nx
-                  !       idx_Hx         = i * (Nx + 1)
-                  !       j = 0
-                  !       !print *, "idx_Hx = ", idx_Hx, "i, j = ", i, j
-                  !       cn%rhs(idx_Hx) = cn%rhs(idx_Hx + 1) 
-                  ! END DO
-
-
-
-
-                  ! Second membre Hy
-                  DO i = 1 , Nx-1
-                        !print *, "i = ", i
-                        DO j = 1,  Ny - 1
-                              ! Détermine le bonne indice
-                              idx_Hy = (Nx+1)*(Ny+1) + i * (Nx + 1) + j
-                              !print *, "idx_Hy = ", idx_Hy, 'i,j =', i , j
-                              ! Calcul du second membre Hy
-                              cn%rhs(idx_Hy) =      (1.d0 - 2.d0 * cn%by**2)*cn%Hy(i,j)                              & 
-                                          + cn%by**2 * ( cn%Hy(i - 1, j) + cn%Hy(i + 1, j)    )                    &
-                                          - cn%bx*cn%by * ( cn%Hx(i , j + 1)  - cn%Hx(i , j)  )                    &
-                                          + cn%bx*cn%by * ( cn%Hx(i-1, j + 1) - cn%Hx(i-1, j) )                    &
-                                          + 2.d0 * cn%a2 * (cn%Ez(i,j) - cn%Ez(i-1, j))
-                        END DO
-                  END DO
-
-                  ! ! Condition aux bords du champ magnétique
-                  ! DO j = 0, Ny
-                  !       idx_Hy = (Nx + 1) * (Ny + 1) + j
-                  !       i = 0
-                  !       !print *, ""
-                  !       !print *, "idx_Hy = ", idx_Hy, "i, j = ", i, j
-                  !       cn%rhs(idx_Hy) = cn%rhs(idx_Hy + 1) 
-                  ! END DO
-
-                                                                           ! Mise à jour explicite de Ez
-                  DO i = 1, Nx-1
-                        DO j = 1, Ny-1
-                              cn%Ez(i,j) = cn%Ez(i,j) - cn%a1 / cn%dy * ( B_pec(i,j + 1) - B_pec(i,j)                      &
-                                                                        + cn%Hx(i, j + 1) - cn%Hx(i,j) )                   &
-                                                      + cn%a1 / cn%dx * ( B_pec(i1 + (i + 1),j) - B_pec(i1 + i,j)          &          ! i1 = Nx + 1
-                                                                        + cn%Hy(i + 1, j) - cn%Hy(i,j) )
-                        END DO
-                  END DO
-
-                  !Injection de source
-                  cn%Ez(i_src,j_src) = Esrc(n)        
-
-                                                      ! CDT DE BORD / PMC
-                  cn%Ez(: ,0)  = 0.d0          ! Bord inférieur
-                  cn%Ez(: ,Ny) = 0.d0          ! Bord supérieur
-                  cn%Ez(0 ,:)  = 0.d0          ! Bord gauche
-                  cn%Ez(Nx,:)  = 0.d0          ! Bord droit
-
-
-                  ! Résolution du système linéaire pour les champs H au temps n + 1
-                  CALL DGETRS('N', SIZE(cn%A,1), nrhs, cn%A, SIZE(cn%A,1), ipiv, cn%rhs, SIZE(cn%rhs), info)
-                  !print *, "pass 4"
-
-                  ! reshape du vecteur rhs / order = [2,1] fait varier j avant i
-                  B_pec = reshape(cn%rhs, shape = [ 2 * (Nx + 1), Ny + 1], order = [2, 1])
-
-
-
-
-                  
-                  ! Ecriture dans le fichier 
-                  IF (MOD(n,snapshot) == 0) THEN
-                        m = m + 1
-                        DO i = 0, Nx
-                              DO j = 0, Ny
-                                    WRITE(idfile + 1, '(F0.15,1X)', advance='no') cn%Ez(i,j)
-                                    !write(idfile    , '(F0.15,1X)', advance='no') cn%Hx(i,j)
-                              END DO
-                              WRITE(idfile + 1, *)
-                              !write(idfile    , *)
-                        END DO 
-                        WRITE(idfile + 1, *)    
-                        WRITE(idfile    , *)
-                  END IF
-                  ! ! !---------------------------------------------------!
-
-
-
-                  
-                  
-            END DO
 
             ! WRITE(*, '(/, T5, A, /)') "Test reshape du vecteur rhs :"
             ! print *, "shape(B_pec) = ", shape(B_pec)
